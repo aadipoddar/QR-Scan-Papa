6b094780db84f8c70322537ed3bdff68d621261a
Aadi Poddar <aadipoddarmail@gmail.com>
2021-10-28 13:04:12 +0530

    Build the Android App to Scan and Store

---
+++ QR-Scan/app/src/main/java/com/example/qrscan/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.qrscan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button qrbtn;
+    public static TextView qrtext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        qrbtn=(Button) findViewById(R.id.qrbtn);
+        qrtext=(TextView) findViewById(R.id.qrtext);
+
+        qrbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),qrscanner.class));
+            }
+        });
+    }
+}
